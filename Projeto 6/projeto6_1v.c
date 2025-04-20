f4b8761fcdaa4918e0592cf04bc109f2fc82cfc8
Juliane Santos <juliane.santos.0200@gmail.com>
2025-04-19 22:08:29 -0300

    p6

---
+++ Projeto 6/projeto6_1v.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+int main() {
+    int i, n = 10000000;
+    int count = 0;
+
+    srand(time(NULL)); // semente fora da região paralela
+
+    #pragma omp parallel for
+    for (i = 0; i < n; i++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+
+        if (x * x + y * y <= 1.0)
+            count++;
+    }
+
+    double pi = 4.0 * count / n;
+    printf("Estimativa incorreta de pi: %f\n", pi);
+
+    return 0;
+}
